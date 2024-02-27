cc723cc0cb5fb582bba83aceeb7c9ce0955a9a45
SELVAPRIYAS <141714142+selvapriya-s14@users.noreply.github.com>
2024-02-27 15:56:38 +0530

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,7 +10,11 @@ public class App {
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println(MESSAGE);
+        int i;
+        for(i=0;i<10;i++)
+            {
+            System.out.println(MESSAGE);
+            }
     }
 
     public String getMessage() {
